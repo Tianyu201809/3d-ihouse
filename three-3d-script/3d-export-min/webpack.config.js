4c0e15ece4dbd2a554f61ab46fcb41fea0132e6f
Lichao <lichao@3dnest.cn>
2022-02-23 09:51:59 +0800

    feat: 新增webpack项目热启动，便于开发和调试

--- three-3d-script/3d-export-min/webpack.config.js
+++ three-3d-script/3d-export-min/webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path")
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
   mode: "development", // 开发模式
   entry: path.resolve(__dirname, "index.js"), // 入口文件
   output: {
-    filename: "[name].[hash:4].js", // 打包后的文件名称
+    filename: "bundle.js", // 打包后的文件名称
     path: path.resolve(__dirname, "./dist"), // 打包后的目录
   },
+  
   plugins: [new CleanWebpackPlugin()],
+  devServer:{
+    port:9000,
+    liveReload: false
+  },
+  node: false
 }
